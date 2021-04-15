8b9d479aaf8cde4bc035dfe06e0ca91dbfc5527b
thantrung0501 <thantrung0501@gmail.com>
2021-04-15 21:49:23 +0700

    Exel DataBase

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 	$servername = "localhost";
-$database = "qlappproject";
+$database = "Laragon\qlapp";
 $username = "root";
-$password = "";
+$password = "123456";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 

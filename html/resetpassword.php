87d6a083435e990882aa778418ecb3073eabe214
KieuTrinh299 <buithikieutrinh1999@gmail.com>
2021-04-16 12:17:14 +0700

    Du an

---
+++ html/resetpassword.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="./css/resetpassword.css">
+	</head>
+<body class="reset_password">
+	<div class="logo">
+		<img src="./images/common/Logo-VNU-1995.jpg" style="max-width:40%;height:auto;">
+		<p>Đăng ký thi đánh giá năng lực
+		<br>
+		Đại học Quốc gia Hà Nội
+		</p>
+		<br>
+		<h5>VNU <small>test center</small></h5>
+	</div>
+	<div class="email">
+		<form action="">
+			<p>Đặt lại mật khẩu</p>
+			<label for="taikhoan">Email:</label><br>
+			<input type="text" id="email" name="email">
+			<input type="submit" value="Gửi yêu cầu">
+		</form>
+	</div>
+</body>
+</html>

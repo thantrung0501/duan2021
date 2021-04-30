ec64913ae5d0c86cc1cd3ded25a1e67e57a54557
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2021-04-30 21:39:35 +0700

    Update GetDataStatisticalForProvince.php

--- action/center/GetDataStatisticalForProvince.php
+++ action/center/GetDataStatisticalForProvince.php
@@ -3,8 +3,10 @@
 	include '../../config.php';
 
 	$provinceName = $_POST["ProvinceName"];
+
+	$FindYear = $_POST["findYear"]?$_POST["FindYear"]:1;
 	// lấy dữ liệu thống kê theo tỉnh
-	$sql = "CALL Pro_GetData_StatisticalForProvince";
+	$sql = "CALL Pro_GetData_StatisticalForProvince($provinceName,$findYear )";
 
 	$query = mysqli_query($conn, $sql);
 	if(mysqli_num_rows($query) > 0){

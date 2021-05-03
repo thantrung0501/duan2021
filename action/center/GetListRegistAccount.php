42d557ff0546211df2c132df1af9cbdac9872b24
Toàn Đặng Trần <43144539+17021065@users.noreply.github.com>
2021-05-03 17:57:13 +0700

    redo

--- action/center/GetListRegistAccount.php
+++ action/center/GetListRegistAccount.php
@@ -12,12 +12,10 @@
 
 		$query = mysqli_query($conn, $sql);
 
-		$listGroupAccount = array();
 		if(mysqli_num_rows($query) > 0){
-			while ($row = mysqli_fetch_array($query)) {
-				array_push($listGroupAccount, $row);
-			}
-			echo json_encode($listGroupAccount);
+
+			$listRegistAccount = mysqli_fetch_array($query);
+			echo json_encode($listRegistAccount);
 		}
 
 	}

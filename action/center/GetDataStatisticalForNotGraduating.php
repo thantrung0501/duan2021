fd33401579f4a211d2823f55d86cad6488b3b2b4
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2021-05-09 09:42:50 +0700

    ptthuy
    bo sung APi lấy những sv ko đạt điều kiện

--- action/center/GetDataStatisticalForNotGraduating.php
+++ action/center/GetDataStatisticalForNotGraduating.php
@@ -1,13 +1,15 @@
 <?php 
   	session_start();
 	include '../../config.php';
+
 	
+
 	$StartYear = $_POST["FindYear"]?$_POST["FindYear"]:1;
 	$EndYear = $_POST["EndYear"]?$_POST["EndYear"]:1;
-	// lấy dữ liệu thống kê đã tốt nghiệp hay chưa
-	$sql = "CALL Pro_GetData_StatisticalForGraduatingYear($StartYear,$EndYear)";
+	// lấy dữ liệu thống kê chưa tốt nghiệp
+
+	$sql = "CALL Pro_GetData_StatisticalForNotGraduatingYear($StartYear,$EndYear)";
 	$query = mysqli_query($conn, $sql);
-	
 	$listGroupAccount = array();
 	if(mysqli_num_rows($query) > 0){
 		while ($row = mysqli_fetch_array($query)) {
@@ -16,5 +18,5 @@
 		echo json_encode($listGroupAccount);
 	}
 	
-	
+
  ?>

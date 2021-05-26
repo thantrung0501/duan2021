89f9b37a3e735dfa591d13b541cc219bb752c230
phamtrungthuy <43264965+thuymax@users.noreply.github.com>
2021-05-26 22:40:27 +0700

    ptthuy
    sửa lại đăng ký thi và đóng

--- action/center/RegistExam/GetListRegistExamDetail.php
+++ action/center/RegistExam/GetListRegistExamDetail.php
@@ -3,7 +3,8 @@
 	session_start();
 	include '../../../config.php';
 	// lấy những thằng có IsRegistAll = 1
-	$sql = "SELECT r.*,r1.* FROM registexamdetail r INNER JOIN registexam r1 on r.RegistExamID = r1.RegistExamID inner join (SELECT if( max(r.ExamDate)< CURRENT_TIMESTAMP ,null, r.RegistExamID) as RegistExamID , max(r.ExamDate) from registexamdetail r GROUP BY r.RegistExamID) r2 on r1.RegistExamID = r2.RegistExamID  ORDER BY r.UnitExam";
+	$sql = "SELECT r.*,r1.* FROM registexamdetail r INNER JOIN registexam r1 on r.RegistExamID = r1.RegistExamID WHERE r1.IsRegistAll = 1 ORDER BY r1.UnitRegist DESC, r.UnitExam";
+	/*$sql = "SELECT r.*,r1.* FROM registexamdetail r INNER JOIN registexam r1 on r.RegistExamID = r1.RegistExamID inner join (SELECT if( max(r.ExamDate)< CURRENT_TIMESTAMP ,null, r.RegistExamID) as RegistExamID , max(r.ExamDate) from registexamdetail r GROUP BY r.RegistExamID) r2 on r1.RegistExamID = r2.RegistExamID  ORDER BY r.UnitExam";*/
 	$query = mysqli_query($conn, $sql);
 
 		$listRegistExam = [];
@@ -13,5 +14,6 @@
 			}
 			echo json_encode($listRegistExam);
 		}
+		
 
  ?>
--- action/student/RegistExam/GetListRegistExamForStudent.php
+++ action/student/RegistExam/GetListRegistExamForStudent.php
@@ -2,10 +2,11 @@
 	session_start();
 	include '../../../config.php';
 	$accountID = $_SESSION["AccountID"];
+	$dateNow = date("Y-m-d H:i:s");
 	// lấy ra danh sách đăng ký thi cho sinh viên
 	// IsRegisted là trường đánh dấu ca nào sinh viên đã đăng ký rồi
 	// IsEqual là trường đánh dấu nếu số lương sinh viên đăng ký bằng số lượng tối đa thì là false và sẽ hiện ra cảnh báo
-	$sql = "SELECT r.*, r2.RegistNumber, r2.CreateYear, if(r1.AccountID = '$accountID', true, false) as IsRegisted , if(r.Examee = r.ExameeMax , true, false) IsEqual from registexamdetail r LEFT JOIN registexaminfor r1 on r.RegistExamDetailID = r1.RegistExamDetailID and r1.AccountID ='$accountID' INNER JOIN registexam r2 on r.RegistExamID = r2.RegistExamID WHERE r2.IsRegistAll = true ORDER BY r2.RegistNumber, r.UnitExam";
+	$sql = "SELECT r.*, r2.*, if(r1.AccountID = '$accountID', true, false) as IsRegisted , if(r.Examee = r.ExameeMax , true, false) IsEqual from registexamdetail r LEFT JOIN registexaminfor r1 on r.RegistExamDetailID = r1.RegistExamDetailID and r1.AccountID ='$accountID' INNER JOIN registexam r2 on r.RegistExamID = r2.RegistExamID WHERE r2.IsRegistAll = true and r2.FinishDate >CURRENT_TIMESTAMP ORDER BY r2.RegistNumber, r.UnitExam" ;
 	$query = mysqli_query($conn, $sql);
 	
 	$listRegistExam = [];
